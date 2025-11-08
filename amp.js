7fe90438014355cd0b2cde1e381dbdd528ba587e
cp0131ks <cp0131ks@ed.ritsumei.ac.jp>
2025-11-08 19:10:27 +0900

    ファイルの追加

---
+++ amp.js
@@ -0,0 +1,404 @@
+define(['pipAPI', 'https://cp0131ks.github.io/study3/qamp.js'], function(APIConstructor, ampExtension){
+
+	var API = new APIConstructor();
+	
+	
+	return ampExtension({
+		primeCats :  [
+			
+			{
+				nameForFeedback : 'Man_1_B',  //Will be used in the user feedback 
+				nameForLogging : 'Man_1_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				       {word : '男性は力が強い'}				
+                  
+				]
+
+			}, 
+			{
+				nameForFeedback : 'Man_2_B',  //Will be used in the user feedback 
+				nameForLogging : 'Man_2_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [ 
+					{word : '男性は筋肉がある'}				
+                  
+				]
+
+			}, 
+			{
+				nameForFeedback : 'Man_3_B',  //Will be used in the user feedback 
+				nameForLogging : 'Man_3_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '男性は体が大きい'}
+
+				]
+
+			}, 
+			{
+				nameForFeedback : 'Man_4_B',  //Will be used in the user feedback 
+				nameForLogging : 'Man_4_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '男性は力がある'}					
+                  
+				]
+
+			}, 
+			{
+				nameForFeedback : 'Man_5_B',  //Will be used in the user feedback 
+				nameForLogging : 'Man_5_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '男性は身長が高い'}					
+                  
+				]
+
+			}, 
+			{
+				nameForFeedback : 'Man_6_C',  //Will be used in the user feedback 
+				nameForLogging : 'Man_6_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '男性は足がクサい'}					
+                  
+				]
+
+			}, 
+			{
+				nameForFeedback : 'Man_7_C',  //Will be used in the user feedback 
+				nameForLogging : 'Man_7_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '男性は虫好きである'}					
+                  
+				]
+
+			}, 
+			{
+				nameForFeedback : 'Man_8_C',  //Will be used in the user feedback 
+				nameForLogging : 'Man_8_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '男性は見栄っ張り'}					
+                  
+				]
+
+			},  
+			{
+				nameForFeedback : 'Man_9_C',  //Will be used in the user feedback 
+				nameForLogging : 'Man_9_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '男性は浮気をする'}					
+                  
+				]
+
+			},  
+			{
+				nameForFeedback : 'Man_10_C',  //Will be used in the user feedback 
+				nameForLogging : 'Man_10_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '男性は不器用である'}					
+                  
+				]
+
+			},  
+			{
+				nameForFeedback : 'Woman_1_B',  //Will be used in the user feedback 
+				nameForLogging : 'Woman_1_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : '女性は力が弱い'}
+				]
+			},
+			{
+				nameForFeedback : 'Woman_2_B',  //Will be used in the user feedback 
+				nameForLogging : 'Woman_2_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [ 
+					{word : '女性は美意識が高い'}
+				]
+			},
+			{
+				nameForFeedback : 'Woman_3_B',  //Will be used in the user feedback 
+				nameForLogging : 'Woman_3_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '女性は虫嫌い'}
+				]
+			},
+			{
+				nameForFeedback : 'Woman_4_B',  //Will be used in the user feedback 
+				nameForLogging : 'Woman_4_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '女性はやわらかい'}
+				]
+			},
+			{
+				nameForFeedback : 'Woman_5_B',  //Will be used in the user feedback 
+				nameForLogging : 'Woman_5_B', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '女性は体が小さい'}
+				]
+			},
+			{
+				nameForFeedback : 'Woman_6_C',  //Will be used in the user feedback 
+				nameForLogging : 'Woman_6_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [ 
+					{word : '女性は裏表が激しい'}
+				]
+			},
+			{
+				nameForFeedback : 'Woman_7_C',  //Will be used in the user feedback 
+				nameForLogging : 'Woman_7_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '女性は運転が嫌い'}
+				]
+			},
+			{	nameForFeedback : 'Woman_8_C',  //Will be use
+				nameForLogging : 'Woman_8_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '女性は妄想癖がある'}
+				]
+			},
+			{	nameForFeedback : 'Woman_9_C',  //Will be use
+				nameForLogging : 'Woman_9_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '女性は群れる'}
+				]
+			},
+			{	nameForFeedback : 'Woman_10_C',  //Will be use
+				nameForLogging : 'Woman_10_C', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '女性はうるさい'}
+				]
+			},
+			{
+				nameForFeedback : 'Neutral_1',  //Will be used in the user feedback 
+				nameForLogging : 'Neutral_1', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'こたつは暖房器具だ'}
+				]
+			},
+			{
+				nameForFeedback : 'Neutral_2',  //Will be used in the user feedback 
+				nameForLogging : 'Neutral_2', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [ 
+					{word : '消しゴムは文房具だ'}
+				]
+			},
+			{
+				nameForFeedback : 'Neutral_3',  //Will be used in the user feedback 
+				nameForLogging : 'Neutral_3', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '車は移動手段だ'}
+				]
+			},
+			{
+				nameForFeedback : 'Neutral_4',  //Will be used in the user feedback 
+				nameForLogging : 'Neutral_4', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '車にはタイヤがある'}
+				]
+			},
+			{
+				nameForFeedback : 'Neutral_5',  //Will be used in the user feedback 
+				nameForLogging : 'Neutral_5', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : 'ドーナツは甘い'}
+				]
+			},
+			{
+				nameForFeedback : 'Neutral_6',  //Will be used in the user feedback 
+				nameForLogging : 'Neutral_6', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [ 
+					{word : 'こたつは冬の定番だ'}
+				]
+			},
+			{
+				nameForFeedback : 'Neutral_7',  //Will be used in the user feedback 
+				nameForLogging : 'Neutral_7', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : 'こたつは冬に使う'}
+				]
+			},
+			{	nameForFeedback : 'Neutral_8',  //Will be use
+				nameForLogging : 'Neutral_8', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : 'ドーナツはお菓子だ'}
+				]
+			},
+			{	nameForFeedback : 'Neutral_9',  //Will be use
+				nameForLogging : 'Neutral_9', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : '川の水は冷たい'}
+				]
+			},
+			{	nameForFeedback : 'Neutral_10',  //Will be use
+				nameForLogging : 'Neutral_10', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : 'ドーナツはおやつだ'}
+				]
+			},
+			
+		
+		],
+
+		examplePrimeStimulus : 
+		{
+			nameForLogging : 'examplePrime', //Will be used in the logging
+			//An array of all media objects for this category.
+			mediaArray : [{word : '傘が開いている'}, {word : 'ランプが光っている'}, {word : '茶色のイスがある'}]
+		},
+		targetCats :  [
+			{
+				nameForFeedback : 'イ音節文字',  //Will be used in the user feedback 
+				nameForLogging : 'イ音節文字', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				     {word : 'ꂊ'},
+				    {word : 'ꃞ'},
+				    {word : 'ꄅ'},
+				    {word : 'ꈁ'},
+				    {word : 'ꉭ'},
+				    {word : 'ꌵ'},
+				    {word : 'ꍯ'},
+				    {word : 'ꎓ'},
+				    {word : 'ꏤ'},
+				    {word : 'ꏸ'},
+				    {word : 'ꐎ'},
+				    {word : 'ꐧ'},
+				    {word : 'ꄰ'},
+				    {word : 'ꅄ'},
+				    {word : 'ꈭ'},
+				    {word : 'ꈲ'},
+				    {word : 'ꋖ'},
+				    {word : 'ꍡ'},
+				    {word : 'ꍤ'},
+				    {word : 'ꍭ'},
+				    {word : 'ꎨ'},
+				    {word : 'ꎰ'},
+				    {word : 'ꏲ'},
+				    {word : 'ꐬ'},
+				    {word : 'ꐸ'},
+				    {word : 'ꀅ'},
+				    {word : 'ꑿ'},
+				    {word : 'ꀱ'},
+				    {word : 'ꂅ'},
+				    {word : 'ꂨ'},
+				    {word : 'ꃡ'},
+				    {word : 'ꃢ'},
+				    {word : 'ꄩ'},
+				    {word : 'ꄸ'},
+				    {word : 'ꅫ'},
+				    {word : 'ꆏ'},
+				    {word : 'ꇜ'},
+				    {word : 'ꇢ'},
+				    {word : 'ꇿ'},
+				    {word : 'ꈍ'},
+				    {word : 'ꈥ'},
+				    {word : 'ꉐ'},
+				    {word : 'ꊲ'},
+				    {word : 'ꊼ'},
+				    {word : 'ꌕ'},
+				    {word : 'ꌙ'},
+				    {word : 'ꍣ'},
+				    {word : 'ꎍ'},
+				    {word : 'ꎖ'},
+				    {word : 'ꐀ'},
+				    {word : 'ꐃ'},
+				    {word : 'ꀡ'},
+				    {word : 'ꀇ'},
+				    {word : 'ꁈ'},
+				    {word : 'ꁌ'},
+				    {word : 'ꂛ'},
+				    {word : 'ꃀ'},
+				    {word : 'ꃤ'},
+				    {word : 'ꅲ'},
+				    {word : 'ꈵ'},
+				    {word : 'ꉘ'},
+				    {word : 'ꉜ'},
+				    {word : 'ꉠ'},
+				    {word : 'ꊥ'},
+				    {word : 'ꌅ'},
+				    {word : 'ꌝ'},
+				    {word : 'ꍉ'},
+				    {word : 'ꏃ'},
+				    {word : 'ꏓ'},
+				    {word : 'ꏙ'},
+				    {word : 'ꏚ'},
+				    {word : 'ꏵ'},
+				    {word : 'ꐰ'},
+				    {word : 'ꑗ'},
+				    {word : 'ꑷ'},
+				    {word : 'ꒁ'},
+				    {word : 'ꀹ'},
+				    {word : 'ꀾ'},
+				    {word : 'ꁕ'},
+				    {word : 'ꀀ'},
+				    {word : 'ꀈ'},
+				    {word : 'ꀒ'},
+				    {word : 'ꀽ'},
+				    {word : 'ꃆ'},
+				    {word : 'ꃛ'},
+				    {word : 'ꄉ'},
+				    {word : 'ꄌ'},
+				    {word : 'ꆹ'},
+				    {word : 'ꆾ'},
+				    {word : 'ꇡ'},
+				    {word : 'ꇱ'},
+				    {word : 'ꈑ'},
+				    {word : 'ꉬ'},
+				    {word : 'ꉱ'},
+				    {word : 'ꊈ'},
+				    {word : 'ꊿ'},
+				    {word : 'ꋄ'},
+				    {word : 'ꋩ'},
+				    {word : 'ꍕ'},
+				    {word : 'ꎎ'},
+				    {word : 'ꎙ'},
+				    {word : 'ꎮ'},
+				    {word : 'ꏋ'},
+				    {word : 'ꏗ'},
+				    {word : 'ꐞ'},
+				    {word : 'ꐥ'},
+				    {word : 'ꎆ'},
+				    {word : 'ꒇ'},
+				    {word : 'ꀵ'},
+				    {word : 'ꁄ'},
+				    {word : 'ꁜ'},
+				    {word : 'ꁮ'},
+
+                  
+				]
+
+			}
+
+		
+		],
+
+		base_url : {//Where are your images at?
+			image : 'https://cp0131ks.github.io/study3/image'
+		}
+	});
+});
